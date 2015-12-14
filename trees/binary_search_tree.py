54462e60c43db40db71c22b4f631403552bb319f
pburakov <pburakov@gmail.com>
2015-12-13 21:18:02 -0500

    added more comments

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -8,7 +8,7 @@ class BinarySearchTree:
      and Children keep back-reference to their parent. That ensures finding
      a value in the Tree in a very short time, greatly reducing number of
      comparisons for well-balanced trees.
-    Binary Search Tree is also very easy to implement using recursion.
+    Binary Search Tree is also fairly easy to implement.
     """
 
     def __init__(self):
@@ -18,6 +18,12 @@ class BinarySearchTree:
         return self.root
 
     def recursive_search(self, node, key):
+        """
+        Basic recursive search. Search can start at any node. Base case: the
+         matching key is found, the search is successful and the Node is returned.
+         This search takes advantage of the Binary Search Tree structure, it "knows"
+         in what direction it has to "dig".
+        """
         if node is None or key == node.key:
             return node
         elif key < node.key:
@@ -26,6 +32,9 @@ class BinarySearchTree:
             return self.recursive_search(node.right, key)
 
     def iterative_search(self, node, key):
+        """
+        Search using simple iteration.
+        """
         current_node = node
         while current_node:
             if key == current_node.key:
@@ -37,6 +46,15 @@ class BinarySearchTree:
         return None
 
     def insert(self, key, value):
+        """
+        Insertion begins as a search would begin. We examine the root and
+         recursively insert the new node to the left subtree if its key is less
+         than that of the root, or the right subtree if its key is greater than
+         the root. This implementation raises an error on duplicate keys, but we
+         could as well replace the value (see replace_node()).
+        This method can also be done recursively similar to recursive_search()
+         given that a reference to the current node will be passed along.
+        """
         if self.root is None:
             self.root = Node(key, value)
             return True
@@ -58,6 +76,9 @@ class BinarySearchTree:
                     return True
 
     def replace_node(self, node, new_node):
+        """
+        Replaces selected node and updates references to itself in the parent.
+        """
         if node == self.root:
             self.root = new_node
             return True
@@ -67,9 +88,18 @@ class BinarySearchTree:
         elif parent.right and parent.right == node:
             parent.right = new_node
         else:
-            raise RuntimeError("Incorrect parent-children relation")
+            # Orphaned node?
+            raise KeyError("Incorrect parent-children relation")
 
     def remove_node(self, node):
+        """
+        Deletion is a little complex. There are 3 possible cases:
+         - Node has two children. Find its in-order successor (left-most node in
+          its right sub-tree), let's call it R. We copy R's key and value to
+          the node, and remove R from its right sub-tree.
+         - Node has one child. The node is deleted and is replaces with its child
+         - Node is a leaf and is simply removed
+        """
         if node.left and node.right:
             successor = node.right
             while successor.left:
@@ -85,6 +115,9 @@ class BinarySearchTree:
             self.replace_node(node, None)
 
     def delete(self, key):
+        """
+        Finds a node and removes it using the algorithm above.
+        """
         node = self.iterative_search(self.root, key)
         if node:
             self.remove_node(node)
@@ -100,7 +133,9 @@ class Node:
 
 
 """
-Tree Traversals with callback functions, first parameter is a starting node.
+In-order Tree Traversals with callback functions. First parameter is a starting
+ node. Pre-order or post-order traversals are done in similar way (see
+ parse_tree_traversal.py), but are not likely to be useful for Binary Search Tree.
 
 Example usage:
     bst = BinarySearchTree()
@@ -124,19 +159,3 @@ def traverse_in_order(node, function):
     traverse_in_order(node.left, function)
     function(node)
     traverse_in_order(node.right, function)
-
-
-def traverse_pre_order(node, function):
-    if node is None:
-        return None
-    function(node)
-    traverse_in_order(node.left, function)
-    traverse_in_order(node.right, function)
-
-
-def traverse_post_order(node, function):
-    if node is None:
-        return None
-    traverse_in_order(node.left, function)
-    traverse_in_order(node.right, function)
-    function(node)
