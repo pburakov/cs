bd790c64fba159e9f1351504674c1382ad7f55b8
pburakov <pburakov@gmail.com>
2016-03-23 23:46:21 -0400

    fixed selection sort (sothing terrible was written here before)

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,14 +1,15 @@
 def selection_sort(list):
     """
-    Selection Sort finds the biggest element in the search window (limited by
-     a starting element and a Right position marker) and swaps it with an element
-     at the Right edge of the search window. One of the least effective search
-     algorithms with constant complexity of O(n^2) in all cases.
+    Selection Sort is a "child's algorithm". It finds the smallest / biggest element
+     in the search window and swaps it with an element in the front. One of the
+     least effective search algorithms.
+
+    Complexity: O(n^2) in all cases.
     """
-    for r_marker in range(len(list) - 1, 0, -1):
-        i_max = 0  # index of a maximum value
-        for i in range(1, r_marker):
-            if list[i] > list[i_max]:
-                i_max = i
-        print('marker at', r_marker, ': swapped', list[r_marker], 'and', list[i_max])
-        list[r_marker], list[i_max] = list[i_max], list[r_marker]
+
+    for i in range(len(list)):
+        for k in range(len(list)):
+            if list[k] > list[i]:
+                print('swapped', list[i], 'and', list[k])
+                list[i], list[k] = list[k], list[i]
+    return list
