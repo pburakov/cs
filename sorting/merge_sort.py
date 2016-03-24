1a8cc5d0c76f1b7727a3deefc832db7eac8c426c
pburakov <pburakov@gmail.com>
2016-03-24 00:03:44 -0400

    refactored merge sort a bit

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -9,35 +9,39 @@ def merge_sort(list):
      Merge Sort requires additional memory to hold the sliced two halves which can
      be critical for larger sets.
     """
-    print('got', list)
+
+    def merge(left, right):
+        """
+        This merge helper is doing the "heavy lifting". Values are picked one by one form `left`
+         and `right` lists.
+        Lists that have more than one element are already sorted, so the first element from
+         each is "picked".
+        """
+        print('merging', left, 'and', right, end=' ')
+        result = []
+        left_i, right_i = 0, 0
+        while left_i < len(left) and right_i < len(right):
+            # Change the direction of this comparison to change the direction of the sort
+            if left[left_i] <= right[right_i]:
+                result.append(left[left_i])
+                left_i += 1
+            else:
+                result.append(right[right_i])
+                right_i += 1
+        # Adds remaining elements
+        if left:
+            result.extend(left[left_i:])
+        if right:
+            result.extend(right[right_i:])
+        print('into', result)
+        return result
+
+    print('splitting', list, 'in half')
     if len(list) <= 1:
-        return list
-
-    middle = len(list) // 2
-    left = list[:middle]
-    right = list[middle:]
-
+        return list  # Base case
+    mid_i = len(list) // 2
+    left = list[:mid_i]
+    right = list[mid_i:]
     left = merge_sort(left)
     right = merge_sort(right)
     return merge(left, right)
-
-
-def merge(left, right):
-    print('merging', left, 'and', right)
-    result = []
-    left_i, right_i = 0, 0
-    while left_i < len(left) and right_i < len(right):
-        # Change the direction of this comparison to change the direction of the sort
-        if left[left_i] <= right[right_i]:
-            result.append(left[left_i])
-            left_i += 1
-        else:
-            result.append(right[right_i])
-            right_i += 1
-
-    if left:
-        result.extend(left[left_i:])
-    if right:
-        result.extend(right[right_i:])
-    print('into', result)
-    return result
