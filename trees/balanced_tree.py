67ca58ba2486a73bde97c6dd43cfa695a1578848
pburakov <pburakov@gmail.com>
2016-03-23 23:26:41 -0400

    completely reworked avl tree

--- trees/balanced_tree.py
+++ trees/balanced_tree.py
@@ -1,5 +1,5 @@
 from trees.binary_search_tree import Node
-from trees.binary_search_tree import *
+from trees.binary_search_tree import insert as bst_insert
 
 """
 AVL tree Node implementation based on Binary Search Tree Node.
@@ -26,18 +26,86 @@ def balance(node):
     :param Node node:
     :return int: For AVL trees -1 <= b <= 1
     """
-    return height(node.left) - height(node.right)
+    if node is None:
+        return 0
+    else:
+        return height(node.left) - height(node.right)
 
-#
-# root = Node(2)
-# insert(Node(7), root)
-# insert(Node(9), root)
-# insert(Node(5), root)
-# insert(Node(1), root)
-# insert(Node(3), root)
-# insert(Node(4), root)
-# insert(Node(6), root)
-# insert(Node(8), root)
-# print_tree(root)
-# print("Root Height", height(root))
-# print("Root Bal", balance(root))
+
+def rotate_right(node):
+    """
+    Right tree rotation.
+
+    :param Node node: current root Node of the rotation
+    :return Node: new root Node after the rotation (pivot node)
+    """
+    new_root = node.left
+    node.left = new_root.right
+    if new_root.right:
+        new_root.right.parent = node
+    new_root.parent = node.parent
+    if node.parent:
+        # Is node left or right child?
+        if node.parent.right is node:
+            node.parent.right = new_root
+        else:
+            node.parent.left = new_root
+    new_root.right = node
+    node.parent = new_root
+    return new_root
+
+
+def rotate_left(node):
+    """
+    Left tree rotation.
+
+    :param Node node: current root Node of the rotation
+    :return Node: new root Node after the rotation (pivot node)
+    """
+    new_root = node.right
+    node.right = new_root.left
+    if new_root.left:
+        new_root.left.parent = node
+    new_root.parent = node.parent
+    if node.parent:
+        # Is node left or right child?
+        if node.parent.right is node:
+            node.parent.right = new_root
+        else:
+            node.parent.left = new_root
+    new_root.left = node
+    node.parent = new_root
+    return new_root
+
+
+def insert(node, root):
+    """
+    Same as BST insert but tree is automatically rebalanced after each insert,
+     and a root of rebalanced tree is returned as it may shift.
+
+    :param Node node: new Node
+    :param Node root: root Node to start with
+    :return Node: root node of a rebalanced tree
+    """
+    bst_insert(node, root)
+    return rebalance(root)
+
+
+def rebalance(node):
+    """
+    Rebalances the tree top-down starting at `node`.
+    Returns the root of a rebalanced tree as it may shift
+
+    :param Node node: root node to start rebalancing
+    :return Node: root node of a rebalanced tree
+    """
+    balance_factor = balance(node)
+    if balance_factor > 1:
+        if balance(node.left) < 0:
+            rotate_left(node.left)
+        node = rotate_right(node)
+    if balance_factor < -1:
+        if balance(node.right) > 0:
+            rotate_right(node.right)
+        node = rotate_left(node)
+    return node
