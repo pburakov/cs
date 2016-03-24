f314e4a5ce6812ec8cd57e75b58ee17dbeb09d8b
pburakov <pburakov@gmail.com>
2016-03-23 23:39:46 -0400

    fixed bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,15 +1,17 @@
 def bubble_sort(list):
     """
     Bubble Sort makes n passes from end to start and swaps neighbour elements
-     if inconsistency is found. Bubble Sort is considered one the most ineffective
-     sorting methods along with Selection Sort as it makes same amount of passes
-     for every element of a list, uses nested loops and makes several swaps
-     per pass.
-    Complexity: best O(n), average/worst O(n^2)
+     if inconsistency is found. Element "bubble" up or down (depending on sorting
+     direction) in the list.
+
+    Bubble Sort is considered one the most ineffective sorting methods along with
+     Selection Sort.
+
+    Complexity: average/worst O(n^2)
     """
-    for pass_num in range(len(list) - 1, 0, -1):
-        print('pass', abs(pass_num - len(list)))
-        for i in range(pass_num):
-            if list[i] > list[i + 1]:
+    for k in range(len(list) - 1):
+        for i in range(len(list) - 1):
+            if list[i] < list[i + 1]:
                 print(' swapped', list[i], 'and', list[i + 1])
-                list[i], list[i + 1] = list[i + 1], list[i]
+                list[i + 1], list[i] = list[i], list[i + 1]
+    return list
