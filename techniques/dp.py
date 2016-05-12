292cf6f96ea8655c64705a69b722849b55e02837
Paul Burakov <pburakov@gmail.com>
2016-05-12 16:07:41 -0400

    split DP and memoization

---
+++ techniques/dp.py
@@ -0,0 +1,12 @@
+"""
+A DP is an algorithmic technique which is usually based on a recurrent formula
+ and one (or some) starting states. A sub-solution of the problem is constructed
+ from previously found ones. DP solutions have a polynomial complexity which
+ assures a much faster running time than other techniques like backtracking,
+ brute-force etc.
+
+Dynamic strategy is often similar to recursive solution that includes caching or
+ other means of storing previously found solutions.
+
+For examples, see dp.py in /exercies folder.
+"""
--- techniques/memoization.py
+++ techniques/memoization.py
@@ -1,16 +1,8 @@
 """
-A DP is an algorithmic technique which is usually based on a recurrent formula
- and one (or some) starting states. A sub-solution of the problem is constructed
- from previously found ones. DP solutions have a polynomial complexity which
- assures a much faster running time than other techniques like backtracking,
- brute-force etc.
+Memoization is considered to be a part of a dynamic approach to solving a problem.
+ Universal memoize function decorator is implemented below.
 
-For examples, see dp.py in /exercies folder.
-
-Dynamic approach is often similar to recursive solution that includes caching
- or memoization strategy. Universal memoize function decorator is implemented below.
-
-For exmples of use of memoization see fibonacci.py in /exercises folder.
+For examples of use of memoization see fibonacci.py in /exercises folder.
 """
 
 
