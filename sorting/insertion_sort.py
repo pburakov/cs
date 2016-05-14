0478f2140ed3ba87d8b8269f555cefa242514b70
Paul Burakov <pburakov@gmail.com>
2016-05-14 16:54:07 -0400

    update to a more illustrative version of insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,21 +1,27 @@
 def insertion_sort(list):
     """
     Insertion Sort traverses the list starting with a second element and assumes
-     that items to the left of the Marker are already sorted. It grabs the next
-     element to the right of the Marker in the list and puts it in the right
-     position in the array to the left of the Marker. Insertion sort generally
-     shows a good performance.
-    Complexity: best O(n), average/worst O(n^2)
+     that items to the left of the Marker are already sorted. It "picks" the next
+     element to the right of the Marker in the list and puts it in its position
+     in the sorted part of an array, iteratively comparing with previous
+     elements.
+
+    The best analogy for insertion sort is sorting a hand of cards. Insertion sort
+     generally shows a good performance on small number of elements.
+
+    Complexity: best O(n), average/worst O(n^2). Insertion Sort swaps elements in
+     place and does not require additional storage.
+
     """
-    for marker in range(1, len(list)):
-        cur_value = list[marker]
-        cur_index = marker
-        print('position', marker, ':', cur_value)
-        while cur_index > 0 and list[cur_index - 1] > cur_value:
-            list[cur_index] = list[cur_index - 1]
-            cur_index -= 1
-        if cur_index != marker:
-            print(' -> put before', list[cur_index])
-        else:
-            print(' -> put after', list[cur_index - 1])
-        list[cur_index] = cur_value
+
+    n = len(list)
+    for k in range(1, n):
+        v = list[k]  # Value of a picked element
+        i = k        # Index of picked element
+
+        while i > 0 and list[i - 1] < v:  # Flip this comparison for reversed order
+            list[i], list[i - 1] = list[i - 1], list[i]
+            i -= 1
+        list[i] = v
+
+    return list
