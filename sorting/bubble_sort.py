9943f8cc7554a8f3613ddb23f97cc1d371ee8e33
Paul Burakov <pburakov@gmail.com>
2016-05-15 15:17:17 -0400

    updated bubble_sort to clsr version

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,17 +1,40 @@
-def bubble_sort(list):
+def bubble_sort(A):
     """
-    Bubble Sort makes n passes from end to start and swaps neighbour elements
-     if inconsistency is found. Element "bubble" up or down (depending on sorting
-     direction) in the list.
+    Bubble Sort repeatedly swaps neighbour elements that are out of order. Elements
+     "bubble" up or down (depending on sorting direction) in the list.
 
-    Bubble Sort is considered one the most ineffective sorting methods along with
-     Selection Sort.
+    Original Bubble Sort is considered one the most ineffective sorting methods.
+     Below is slightly "optimized" version of the algorithm that iterates backwards
+     over the unsorted part, that is similar to Insertion sort.
 
-    Complexity: average/worst O(n^2)
+    Complexity: best O(n), average/worst O(n^2). Bubble sort is a stable algorithm
+     and performs an in-place sorting.
+    :param A: Array (list) to sort
+    :return: None. List `A` is mutated.
     """
-    for k in range(len(list) - 1):
-        for i in range(len(list) - 1):
-            if list[i] < list[i + 1]:
-                print(' swapped', list[i], 'and', list[i + 1])
-                list[i + 1], list[i] = list[i], list[i + 1]
-    return list
+    n = len(A)   # Size of the array
+    i_n = n - 1  # Index of last element
+
+    for k in range(0, i_n):
+        for i in range(i_n, k, -1):
+            if A[i] < A[i - 1]:  # Flip this comparison for reversed order
+                A[i], A[i - 1] = A[i - 1], A[i]
+
+
+def bubble_sort_orig(A):
+    """
+    Original (unoptimized) version of Bubble Sort that takes quadratic time in all
+     cases as it will compare every element n^2 times, even if the list is already
+     sorted.
+
+    Complexity: O(n^2) in all cases.
+    :param A: Array (list) to sort
+    :return: None. List `A` is mutated.
+    """
+    n = len(A)   # Size of the array
+    i_n = n - 1  # Index of last element
+
+    for k in range(0, i_n):
+        for i in range(0, i_n):
+            if A[i] > A[i + 1]:  # Flip this comparison for reversed order
+                A[i], A[i + 1] = A[i + 1], A[i]
--- sorting_demo.py
+++ sorting_demo.py
@@ -1,18 +1,19 @@
 from sorting.insertion_sort import insertion_sort
 from sorting.merge_sort import merge_sort
 from sorting.heap_sort import heap_sort
+from sorting.bubble_sort import bubble_sort
 
 l = [4, 7, 5, 2, 3, 9, 6, 1, 8]
-
-# Insertion Sort sorts array in place and has no output
 insertion_sort(l)
 print(l)
 
 l = [4, 7, 5, 2, 3, 9, 6, 1, 8]
-# Merge Sort yields a new array
 print(merge_sort(l))
 
 l = [4, 7, 5, 2, 3, 9, 6, 1, 8]
-# Heap Sort performs in place sorting
 heap_sort(l)
+print(l)
+
+l = [4, 7, 5, 2, 3, 9, 6, 1, 8]
+bubble_sort(l)
 print(l)
