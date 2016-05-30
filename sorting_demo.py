aa0de7274cb6019ddc932309b38360ee0349cf12
Paul Burakov <pburakov@gmail.com>
2016-05-29 23:37:36 -0400

    added counting sort to demo

--- sorting_demo.py
+++ sorting_demo.py
@@ -2,6 +2,7 @@ from sorting.insertion_sort import insertion_sort
 from sorting.merge_sort import merge_sort
 from sorting.heap_sort import heap_sort
 from sorting.bubble_sort import bubble_sort
+from sorting.counting_sort import counting_sort
 
 l = [4, 7, 5, 2, 3, 9, 6, 1, 8]
 insertion_sort(l)
@@ -17,3 +18,6 @@ print(l)
 l = [4, 7, 5, 2, 3, 9, 6, 1, 8]
 bubble_sort(l)
 print(l)
+
+l = [4, 3, 1, 3, 2]
+print(counting_sort(l, max(l)))
