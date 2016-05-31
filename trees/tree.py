681490d31e935ca3629f98bb26d9ed9bffb1d58d
Paul Burakov <pburakov@gmail.com>
2016-05-30 20:57:12 -0400

    removed unimplemented structures

--- trees/tree.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    """
-    Node of a simple tree, with unlimited amount of children.
-    """
-
-    def __init__(self, value):
-        self.value = value
-        self.children = []
