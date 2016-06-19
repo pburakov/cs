89edeedfd4a926e0e0c8ba758ce684f330dadcbf
Paul Burakov <pburakov@gmail.com>
2016-06-19 18:05:54 -0400

    renamed recursive exercise

--- exercises/hop_steps.py
+++ exercises/hop_steps.py
@@ -31,40 +31,9 @@ def count_ways(n):
         return count_ways(n - 1) + count_ways(n - 2) + count_ways(n - 3)
 
 
-print("\n-- Hopping the stairs --")
 print(count_ways(1))
 print(count_ways(2))
 print(count_ways(3))
 print(count_ways(5))
 print(count_ways(7))
 print(count_ways(25))
-
-
-def move_tower(h, from_pole, to_pole, with_pole):
-    """
-    This is a simple recursive solution for Tower Of Hanoi puzzle.
-     More info: https://en.wikipedia.org/wiki/Tower_of_Hanoi
-
-    Initiates a recursive Tower Of Hanoi algorithm to move h disks between
-     first two poles via the third pole. The tree of required actions is printed
-     to the console.
-    Caution: number of actions grows exponentially for each step of h. Use of larger
-     h values may overwhelm the output.
-
-    :param h: Height of the original (ordered) pyramid
-    """
-    if h >= 1:
-        move_tower(h - 1, from_pole, with_pole, to_pole)
-        move_disk(from_pole, to_pole)
-        move_tower(h - 1, with_pole, to_pole, from_pole)
-
-
-def move_disk(fp, tp):
-    """
-    Prints the moving action.
-    """
-    print("moving disk from", fp, "to", tp)
-
-
-print("\n-- Tower of Hanoi --")
-move_tower(3, "A", "B", "C")
