8f587b57981e2718a34ad36d13f488ae1e629a5b
Paul Burakov <pburakov@gmail.com>
2016-06-19 22:41:04 -0400

    minor clarification on variables

--- graphs/graph.py
+++ graphs/graph.py
@@ -2,6 +2,9 @@
 Graph is a linked data structure comprised of vertices (nodes or points) and edges
  (lines, arrows or arcs) representing relationships between vertices.
 
+Graphs can be directed and undirected, their edges can form cycles, sometimes a vertex
+ can point to itself. Directed acyclic graph are often short-named "dags".
+
 Every finite undirected graph has an even number of vertices with odd degree. This
  is so called handshaking lemma, so named due to a notorious example. In a party of
  people some of whom shake hands, an even number of people must have shaken an odd
@@ -9,10 +12,11 @@ Every finite undirected graph has an even number of vertices with odd degree. Th
 
 There are several ways to represent a graph. Most commonly used are adjacency list
  (array) and adjacency matrix (2D array) representations. This implementation uses
- vertex objects with adjacency list stored as an attribute of vertex. Adjacency
- list can be used to represent both directed and undirected graph types. For an
- undirected graph, relation between adjacent vertices is always mutual, while in
- a directed graph, it is not necessarily the case.
+ vertex objects with adjacency list stored as an attribute of vertex.
+
+Adjacency list can be used to represent both directed and undirected graph types.
+ For an undirected graph, relation between adjacent vertices is always mutual, while
+ in a directed graph, it is not necessarily the case.
 """
 
 
@@ -28,12 +32,13 @@ class Vertex:
         :param Any key: Key (value) held by a vertex
         """
         self.key = key
-        self.Adj = []  # List of pointers to adjacent vertices.
+        self.Adj = []  # Adjacency list for the vertex. Can be used to represent
+                       # ...both directed and undirected graph types.
         self.p = None  # Pointer to a parent vertex (from which it was visited)
         self.d = None  # Distance to the vertex from starting vertex (in BFS)
-                       # ...or time (ticker) at which it was discovered (DFS)
+                       # ...or time (ticker) at which it was discovered (DFS).
         self.f = None  # Time (ticker) at which all adjacent vertices have been
-                       # discovered (DFS has finished the vertex)
+                       # discovered (DFS has finished the vertex).
         self.color = None
 
     def degree(self):
@@ -53,4 +58,4 @@ class Graph:
         """
         Basic graph adjacency list graph representation
         """
-        self.V = []  # Adjacency list of all vertices in a graph
+        self.V = []  # List of all vertices in a graph
