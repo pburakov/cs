627c9aa55cb9383d8667c577ef844fe0d941c03b
Paul Burakov <pburakov@gmail.com>
2016-07-03 13:54:25 -0400

    updated description

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -3,17 +3,16 @@ def merge_sort(A):
     Sorts an array of objects using merge-sort algorithm and returns sorted array.
 
     Merge sort is a recursive sorting algorithm that first recursively splits an array
-     in half until it reaches two arrays containing a single element (that are already
-     sorted by definition). Halves are sorted and merged until an entire array is
-     merged back together.
+     in half until it reaches two arrays containing a single element, that are already
+     sorted by definition.
 
     The recursive nature of merge sort is a great example of divide-and-conquer technique,
-     when the problem is broken to smaller pieces each of which is [more or less]
-     trivially solved. The solved parts are combined back together.
+     when the problem is broken to smaller pieces each of which is trivially solved.
+     Solved parts are combined back together.
 
-    `merge(A)` method does the both the "conquer" and "combine" part, merging two sorted
-     arrays. The body of the `merge_sort()` method implements the recursive "divide",
-     or the slicing of the array.
+    Merge operation does the both the "conquer" and "combine" part, merging two sorted
+     arrays. The body of the merge-sort method implements the recursive "divide", or
+     the slicing of the array.
 
     Complexity: O(n log(n)) in all cases. `Log(n)` for splitting, `n` for merge. Merge
      sort is stable and it requires additional memory to hold the sliced halves.
@@ -34,14 +33,20 @@ def merge(L, R):
     """
     Merges two sorted arrays into a single array and returns it.
 
+    The algorithm repeatedly looks at elements at the two pointers, picks the one that
+     is smaller and adds it to the output. The respective pointer is then shifted one
+     position and loop is repeated.
+
+    The merge routine is widely used in other algorithms in various optimized versions.
+
     Complexity: O(k), where `k` is the sum of the length of two arrays.
     :param list L: Sorted array
     :param list R: Sorted array
     :return list: Merged product of arrays `L` and `R`
     """
     s = len(L) + len(R)  # Final size of the product of the merge
-    P = [object] * s     # Allocated memory for the product of the merge
-    i_l, i_r = 0, 0      # Pointers for arrays `L` and `R`
+    P = [object] * s  # Allocated memory for the product of the merge
+    i_l, i_r = 0, 0  # Pointers for arrays `L` and `R`
     max_l, max_r = len(L) - 1, len(R) - 1  # Upper bounds for the pointers
     for i in range(0, s):
         if i_l > max_l:  # Left pointer went out of bounds
