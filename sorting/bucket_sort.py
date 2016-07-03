450441723c984e12822b53235324f64a094aa104
Paul Burakov <pburakov@gmail.com>
2016-07-03 13:36:41 -0400

    added bucket sort

---
+++ sorting/bucket_sort.py
@@ -0,0 +1,32 @@
+def bucket_sort(A):
+    """
+    Sorts an array of integers using bucket sort algorithm and returns sorted array.
+
+    Bucket sort is fast because it makes certain assumptions about the input. It is
+     efficient when input is drawn from a uniform distribution. Evenly distributed
+     items can be sorted into individual buckets and then drawn back in order.
+     Depending on the nature of the input additional sorting routines may be
+     performed on each bucket.
+
+    This implementation of bucket sort assumes that, for `n`-sized array `A`, items
+     are uniformly distributed integers across an interval {0..n}. In order to
+     maintain stability of the sort, buckets are represented by dynamic LIFO
+     structures or stacks.
+
+    Complexity: O(n) in average case for uniformly or evenly distributed input
+    :param list[int] A: Input array
+    :return list[int]: Sorted array
+    """
+    n = len(A)
+    P = [int] * n   # Allocated memory for the sorted output
+    B = [None] * n  # Array of buckets
+    for i in A:
+        if B[i] is None:
+            B[i] = []
+        B[i].append(i)
+    i = n - 1
+    for j in range(n - 1, -1, -1):  # Going backwards to maintain stability
+        while B[j]:
+            P[i] = B[j].pop()
+            i -= 1
+    return P
