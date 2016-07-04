4c75e323f3a9a77f3e27275932ece41f24fb5150
Paul Burakov <pburakov@gmail.com>
2016-07-04 00:11:33 -0400

    added link to description

--- solutions/tower_of_hanoi.py
+++ solutions/tower_of_hanoi.py
@@ -3,26 +3,27 @@ def tower_of_hanoi(P):
     Tower of Hanoi solver.
 
     Tower of Hanoi is a classic problem that is often used to demonstrate recursion.
-     Here's a simple solution for two disks:
+     Conditions are described here: [https://en.wikipedia.org/wiki/Tower_of_Hanoi
 
-    Let `A` be the original rod, `B` the destination and `C` the auxiliary rod.
-     - pick from `A`, put on `C`
-     - pick another disk from `A`, put directly onto `B`
-     - pick disk from `C`, put on `B`
+    Solution for two disks is simple. Let `A` be the original rod, `B` the destination
+     and `C` the auxiliary rod. Problem can be solved in following steps.
+     1. pick from `A`, put on `C`
+     2. pick another disk from `A`, put directly onto `B`
+     3. pick disk from `C`, put on `B`
 
     However, if we add another disk into the problem (`h=4`), we're going to need to
      generalize the approach.
 
     First we need to solve Tower of Hanoi problem for rods `A` and `C` and `h-1` (or
-     first three disks), which is recursive step 1. We're going to need to use rod
-     `B` as an auxiliary.
+     first three disks), which is recursive step 1. We're going to need to use rod `B`
+     as an auxiliary.
 
-    Then we move the last remaining disk from A to B (step 2). After that we solve
-     Tower of Hanoi for remaining disks or `h-1-1` for rods `C` and `B`, this time
-     using rod A as an auxiliary (recursive step 3).
+    Then we move the last remaining disk from A to B (step 2). After that we solve Tower
+     of Hanoi for remaining disks or `h-1-1` for rods `C` and `B`, this time using rod
+     `A` as an auxiliary (recursive step 3).
 
-    `h=1` becomes our base case when we halt the recursion and proceed to the next
-    step, which means that we need to keep track of the height of the tower.
+    `h=1` becomes our base case when we halt the recursion and proceed to the next step,
+     which means that we need to keep track of the height of the tower.
 
     Related sections: Combinatorial Search, Recursion.
 
