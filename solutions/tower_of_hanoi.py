1ff90c6e1c73d5d08f22f0aa2a254821415e087a
Paul Burakov <pburakov@gmail.com>
2016-07-03 22:58:06 -0400

    added time complexity

--- solutions/tower_of_hanoi.py
+++ solutions/tower_of_hanoi.py
@@ -2,8 +2,10 @@ def tower_of_hanoi(P):
     """
     Tower of Hanoi solver.
 
-    Classic recursive problem. Here's a simple solution for two disks:
-     Let A be the original rod, B the destination, C auxiliary rod.
+    Tower of Hanoi is a classic problem that is often used to demonstrate recursion.
+     Here's a simple solution for two disks:
+
+    Let A be the original rod, B the destination, C auxiliary rod.
      - pick from A, put on C
      - pick another disk from A, put directly onto B
      - pick disk from C, put on B
@@ -11,18 +13,18 @@ def tower_of_hanoi(P):
     However, if we add another disk into the problem, we're going to need to
      generalize the approach.
 
-    First we need to solve Tower of Hanoi problem for rods `A` and `C` for `h-1` or
-     3 first disks (recursive step 1) using rod `B` as an auxiliary.
-
-    Then we move the last remaining disk from A to B (step 2).
-
-    After that we solve Tower of Hanoi for remaining disks or `h-1-1` for rods `C`
-     and `B`, this time using rod A as an auxiliary (recursive step 3).
+    First we need to solve Tower of Hanoi problem for rods `A` and `C` for `h-1`
+     (or 3 first disks), which is recursive step 1) using rod `B` as an auxiliary.
+     Then we move the last remaining disk from A to B (step 2). After that we solve
+     Tower of Hanoi for remaining disks or `h-1-1` for rods `C` and `B`, this time
+     using rod A as an auxiliary (recursive step 3).
 
     `h=1` becomes our base case when we halt the recursion and proceed to the next
-    step.
+    step, which means that we need to keep track of the height of the tower.
 
-    :param list[str] P: origin pole
+    Complexity: O(2^n). The time and space complexity is bounded by the number of
+     moves, which grows exponentially with every added disk.
+    :param list[str] P: Origin pole
     :return None: Prints the output
     """
     h = len(P)
@@ -33,6 +35,7 @@ def move_disk(P1, P2):
     """
     Moves disk from Pole 1 to Pole 2, represented as a list.
 
+    Complexity: O(1)
     :param list[str] P1: origin pole
     :param list[str] P2: target pole
     :return None: P1 and P2 are updated
@@ -44,6 +47,7 @@ def move_tower(h, A, B, C):
     """
     Recursive Tower of Hanoi solver that prints out the state after each move.
 
+    Complexity: O(2^n)
     :param int h: height of pile
     :param list[str] A: origin pole
     :param list[str] B: target pole
@@ -56,5 +60,3 @@ def move_tower(h, A, B, C):
         move_disk(A, B)
         print(A, B, C)
         move_tower(h - 1, C, B, A)
-
-tower_of_hanoi(list("123"))
