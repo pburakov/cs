9638d4e321e0bbe7ceaae3d32bf0d434533d30df
Paul Burakov <pburakov@gmail.com>
2016-07-09 23:58:04 -0400

    added clarification comments removed testcase

--- solutions/dp/lis.py
+++ solutions/dp/lis.py
@@ -6,42 +6,39 @@ def lis(A):
      continuous elements of an input set in order of their appearance. Single element is
      LIS by definition.
 
-    Example: `A=[2, 4, 3, 5, 1, 7, 6, 9, 8]`, `output=[2, 3, 5, 6, 8]`.
+    Example: `A=[2, 4, 3, 5, 1, 7, 6, 9, 8, 6]`, `output=[2, 3, 5, 6, 8]`.
 
     Let's jump straight to DP rather than maintaining the state of a recursive algorithm.
      It may seem like there's a lot of magic going on, but really, it's all about building
-     this table:
+     the following DP table:
      ```
-             Index i: 0 1 2 3 4 5 6 7 8 9
-               Input: 2 4 3 5 1 7 6 9 8 3
-     L[i] (LIS at i): 1 2 2 3 1 4 4 5 5 2
+              Index i: 0 1 2 3 4 5 6 7 8 9
+                Input: 2 4 3 5 1 7 6 9 8 6
+     DP[i] (LIS at i): 1 2 2 3 1 4 4 5 5 4
      ```
-    Output LIS can be constructed by traversing this DP table backwards.
+    Output LIS can be constructed by traversing `DP` table backwards.
 
     Complexity: O(n) and O(n+p) space for DP tables.
     :param list A: Input set (list) of comparable objects
     :return list: Output subsequence
     """
     n = len(A)
-    L = [1] * n  # `L[i]` is the longest subsequence seen so far at element `i`.
+    DP = [1] * n  # `DP[i]` is the longest subsequence seen so far at element `i`.
     max_lis = 0  # Length of maximum found found LIS
     for i in range(1, n):
         for j in range(0, i):
-            if A[i] > A[j] and L[i] < L[j] + 1:
-                L[i] = L[j] + 1
-        if L[i] >= max_lis:
-            max_lis = L[i]
+            # Comparing `DP[i]` and `DP[j]+1` ensures that we increase the max LIS
+            # for the subsequence that would include `DP[i]`
+            if A[i] > A[j] and DP[i] < DP[j] + 1:
+                DP[i] = DP[j] + 1
+        if DP[i] >= max_lis:
+            max_lis = DP[i]  # Keeping track of maximum LIS
     # Backtracking on the results of the calculation
     P = [object] * max_lis  # Allocating memory for the output
     j = max_lis - 1  # Index in output list
     for i in range(n - 1, -1, -1):
-        if L[i] == max_lis:
+        if DP[i] == max_lis:
             P[j] = A[i]
             max_lis -= 1
             j -= 1
     return P
-
-
-a1 = [2, 4, 3, 5, 1, 7, 6, 9, 8, 3]
-print(a1)
-print(lis(a1))
