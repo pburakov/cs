5a772ffb310d6dc5f2843d4ffb4e826b4a790b65
Paul Burakov <pburakov@gmail.com>
2016-07-16 21:52:07 -0400

    added problem description

--- solutions/arrays/nuts_bolts.py
+++ solutions/arrays/nuts_bolts.py
@@ -1,30 +1,51 @@
-def nuts_bolts(N, B, q=0, r=None):
+"""
+Nuts and Bolts problem is also known as Lock and Key or Red and Blue Water Jugs problem.
+ The task is to sort values in two arrays `N` and `B` with one constraint. The values
+ from the same array cannot be compared. Value from array `N` can only be compared to a
+ value in array `B`. Arrays are of the same length and contain same elements but in
+ different order. Sort arrays `N` and `B` in place.
+"""
+
+
+def solution(N, B):
     """
     Nuts and Bolts problem solver.
 
-    Also known as Lock and Key or Red and Blue Water Jugs problem. Sort values in two
-     arrays with one constraint. The values from the same array cannot be compared.
-     Value from array `N` can only be compared to a value in array `B`. Arrays are of
-     the same length and contain same elements but in different order.
+    Arrays may consist of any comparable objects: chars, strings, ints or floats.
 
-    Solution uses quicksort sorting algorithm with modified partitioning scheme that
-     takes arbitrary pivot value and finds a match in the array.
+    :param list[object] N: List of nuts
+    :param list[object] B: List of bolts
+    :return None: `N` and `B` are sorted
+    """
+    r = len(B) - 1
+    nb_quicksort(N, B, 0, r)
+
+
+"""
+Auxiliary routines used in the solution
+"""
+
+
+def nb_quicksort(N, B, q=0, r=-1):
+    """
+    Modified quicksort algorithm that solves Nuts and Bolts problem.
+
+    This is basically a quicksort algorithm with modified partitioning scheme that takes
+     an arbitrary pivot value and finds a match in the array, just like finding a single
+     matching "bolt" to a "nut".
 
     Complexity: O(n log(n)), implementation of quick sort algorithm.
     :param list[object] N: List of nuts
     :param list[object] B: List of bolts
     :param int q: Lower bound of sorting (default is 0)
-    :param int r: Upper bound of sorting (default is |B|)
-    :return tuple: Sorted `N` and `B`
+    :param int r: Upper bound of sorting (default is |B|-1)
+    :return None: `N` and `B` are sorted
     """
-    if r is None:
-        r = len(B) - 1
     if q < r:
         p = partition(N, B[r], q, r)
         partition(B, N[p], q, r)
-        nuts_bolts(N, B, q, p - 1)
-        nuts_bolts(N, B, p + 1, r)
-    return N, B
+        nb_quicksort(N, B, q, p - 1)
+        nb_quicksort(N, B, p + 1, r)
 
 
 def partition(S, x, q, r):
