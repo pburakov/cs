6618fa1b0503678309f6a7165e368666ca5fd44e
Paul Burakov <pburakov@gmail.com>
2016-08-03 09:05:24 -0400

    description of naive approach

--- string_matching/naive.py
+++ string_matching/naive.py
@@ -2,7 +2,9 @@ def find(T, P):
     """
     Naive string matching algorithm.
 
-    Returns index of first occurrence of a matching pattern string with target string.
+    Brute-force search with iteration through string `T`. Least effective when pattern
+     `P` contains repeated characters, does increased amount of unnecessary work
+     analyzing spurious hits.
 
     Complexity: O((n - m + 1) m). `n - m + 1` for the main loop and `m` for every
      character comparison within a pattern.
@@ -14,6 +16,6 @@ def find(T, P):
     m = len(P)
     out = []
     for s in range(0, n - m + 1):
-        if P[0:m] == T[s:s + m]:
+        if P[0:m] == T[s:s + m]:  # `m` character comparisons
             out.append(s)
     return out
