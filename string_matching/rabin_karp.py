1bba5cc454b6a3f587d6c15d001fb429650bf359
Paul Burakov <pburakov@gmail.com>
2016-08-03 09:34:50 -0400

    description of Rabin-Karp

--- string_matching/rabin_karp.py
+++ string_matching/rabin_karp.py
@@ -2,10 +2,30 @@ def find(T, P, d=256, q=239):
     """
     Rabin-Karp string matching algorithm.
 
-    Returns index of first occurrence of a matching pattern string with target string.
+    This optimized algorithm reduces the amount of spurious hits over the naive approach
+     by making use of character-based rolling hash.
 
-    Complexity: O((n - m + 1) m), however the algorithm perform much better on average
-     case, due to lesser amounts of spurious hits, with correctly chosen `d` and `q`.
+    First it calculates the hash of the pattern. As it rolls the search window against
+     the string `T`, it copmutes the hash of the characters within that window in an
+     effective way. If both hashes match, finally it performs character-to-character
+     comparison to rule out false-positives.
+
+    Hash function takes advantage of the observation how characters shift in a rolling
+     window. Aside from one character rolling out of focus and one appearing, most of
+     the characters within the window remain the same. Hash function of a window can be
+     written as:
+    ```
+    h(w) = -h(cr) + h(c0) + h(c1) ... + h(cm-1) + h(cm)
+              ^                                    ^
+      removed character                      new character
+    ```
+    The number of spurious hits is less probable with the better uniform distribution of
+     a hash function. Such distribution is achieved with the use of base `d` comparable
+     to a cardinality of a character set being used and a large prime number `q`.
+
+    Complexity: O((n - m + 1) m) worst case, however the algorithm performs much better
+     on average case (O(n+m)), due to lesser amounts of spurious hits, with correctly
+     chosen `d` and `q`.
     :param str T: Target string (haystack)
     :param str P: Pattern string (needle)
     :param int d: Cardinality of a character set (default is 256, corresponding to ASCII)
@@ -14,7 +34,7 @@ def find(T, P, d=256, q=239):
     """
     n = len(T)
     m = len(P)
-    h = pow(d, m - 1) % q
+    h = pow(d, m - 1) % q  # May overflow for large `d`
     p = 0
     t = 0
     out = []
@@ -22,7 +42,7 @@ def find(T, P, d=256, q=239):
         p = (d * p + ord(P[i])) % q
         t = (d * t + ord(T[i])) % q
     for s in range(0, n - m + 1):
-        if p == t:
+        if p == t:  # Candidate is found
             if P[0:m] == T[s:s + m]:
                 out.append(s)
         if s < n - m:
