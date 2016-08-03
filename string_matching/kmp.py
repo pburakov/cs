be3813b2592e4440a37f335d6d2eae30c5dbb0e6
Paul Burakov <pburakov@gmail.com>
2016-08-03 00:47:06 -0400

    added KMP description

--- string_matching/kmp.py
+++ string_matching/kmp.py
@@ -2,7 +2,17 @@ def find(T, P):
     """
     Knuth-Morris-Pratt string matching algorithm.
 
-    Complexity: O(n)
+    This optimized string matching algorithm is based on idea of string matching
+     automata, which examine each text character exatly once. Once a matching character
+     is found, the machine transitions to a pattern matching state and remains in such
+     while the next character is also accepted.
+
+    When we are in the matching state and a potential pattern match is rejected, but
+     another patern occurrence can begin with the previously matched prefix, we can roll
+     back our pattern to the last matching character and continue our evaluation, without
+     shifting the target string.
+
+    Complexity: O(n) amortized.
     :param str T: Target string (haystack)
     :param str P: Pattern string (needle)
     :return list[int]: List of indices where pattern `P` occurs in a target string
@@ -14,12 +24,12 @@ def find(T, P):
     out = []
     for i in range(0, n):
         while q > 0 and P[q] != T[i]:
-            q = π[q - 1]
+            q = π[q - 1]  # Next character does not match
         if P[q] == T[i]:
-            q += 1
+            q += 1  # Next character matches
         if q == m:
             out.append(i - m + 1)
-            q = 0
+            q = 0  # All of the `P` matched, reset automata
     return out
 
 
@@ -27,8 +37,13 @@ def kmp_prefix_function(P):
     """
     Constructs prefix function for use in Knuth-Morris-Pratt algorithm.
 
-    This procedure precomputes a partial match table `π` where `π[q]` is the length of
-     the longest prefix of pattern `P` that is a proper suffix of `P[q]`.
+    This procedure precomputes a partial match table `π`, where `π[q]` is the length of
+     the longest prefix of pattern `P` that is a proper suffix of `P[q]`. Note that both
+     procedures have much in common, because both match a string against the pattern,
+     only prefix function encapsulates knowledge about how the pattern matches against
+     itself.
+
+    Example table for `P="ababaca"`: `π=[0, 0, 1, 2, 3, 0, 1]`.
 
     Complexity: O(m) amortized.
     :param str P: Pattern string
