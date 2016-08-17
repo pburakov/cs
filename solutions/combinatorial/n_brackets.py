b3b856243ce19bfe2d600043930175db14c32876
Paul Burakov <pburakov@gmail.com>
2016-08-17 09:52:47 -0400

    added n-brackets

---
+++ solutions/combinatorial/n_brackets.py
@@ -0,0 +1,28 @@
+"""
+Write a function that prints all possible combinations of `n` well formed bracket pairs.
+
+This is combinatorial problem whose solution is given by the Catalan numbers.
+"""
+
+
+def recursion(C, o, c=0, S=''):
+    """
+    Recursive solution for the n bracket pairs problem.
+
+    Recursive formula is straightforward. Every function call will spawn two recursive
+     calls, one with added opening bracket, another one with the closing bracket, if
+     applicable.
+
+    Complexity: ??? is closely related to formula for catalan numbers.
+    :param int o: Starting number or number of remaining opening brackets
+    :param int c: Number of brackets remaining to be closed (used in recursion)
+    :param str S: Intermediate result (used in recursion)
+    :return None: Prints to stdout
+    """
+    if o == c == 0:
+        print(S)
+        return
+    if o > 0:
+        recursion(C, o - 1, c + 1, S + '(')
+    if c > 0:
+        recursion(C, o, c - 1, S + ')')
