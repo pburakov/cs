386657debbcc7abb7cf95815514ec64b5d7e7d90
Paul Burakov <pburakov@gmail.com>
2016-08-17 22:53:28 -0400

    added description and comments

--- solutions/arrays/merge_intervals.py
+++ solutions/arrays/merge_intervals.py
@@ -1,10 +1,15 @@
 """
- [ - - - - - - - - -]
-   1---3       7---9
-   1-------5
-     2---4
-       3-----6
-
+Given a list of tuples, where each tuple is two integers `(start, end)`, merge
+ overlapping intervals:
+    ```
+     [ - - - - - - - - -]
+       1---3       7---9
+       1-------5
+         2---4
+           3-----6
+    ```
+Input: `[(1, 3), (3, 6), (1, 5), (2, 4), (7, 8)]`
+Output: `[(1, 6), (7, 8)]`
 """
 
 
@@ -12,7 +17,12 @@ def solution(L):
     """
     Returns list of merged intervals.
 
-    Complexity: O(n log(n)), array is sorted using Python's timsort
+    Solution is pretty straightforward. Algorithm traverses the array and looks if the
+     next interval starts before current one ends. Special care should be taken when
+     preparing edge cases for testing.
+
+    Complexity: O(n log(n)), array is sorted using Python's timsort, O(1) space for
+     keeping single current interval in memory.
     :param list[tuple]] L: Non-empty list of intervals
     :return list[tuple]: List of merged intervals
     """
@@ -21,7 +31,7 @@ def solution(L):
     x, y = X[0]
     for a, b in X:
         if a <= y:
-            y = max(b, y)
+            y = max(b, y)  # Extending the interval or keeping the boundary
         else:
             out.append((x, y))
             x = a
