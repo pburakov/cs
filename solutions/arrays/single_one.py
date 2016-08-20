aab49b5a1c715f42b176b3b2721fb560e90cbb1a
Paul Burakov <pburakov@gmail.com>
2016-08-20 09:53:54 -0400

    added comments

--- solutions/arrays/single_one.py
+++ solutions/arrays/single_one.py
@@ -1,5 +1,6 @@
 """
-Given an array of integers, every element appears twice except for one. Find that single one.
+Given an array of integers, every element appears twice except for one. Find that single
+ one.
 
 Input : [1, 2, 2, 3, 1]
 Output : 3
@@ -8,16 +9,20 @@ Output : 3
 
 def freq_map(A):
     """
-    Complexity: O(n) time, O(n) space for frequence  map
+    Linear solution using frequency map.
+
+    Straightforward algorithm that scans the array and constructs a frequency map.
+
+    Complexity: O(n) time, O(n) space for frequency map
     :param list[int] A: Input array
-    :return int:
+    :return int: Missing number
     """
     M = {}
     for i in A:
         if i not in M:
             M[i] = 0
         M[i] += 1
-    for i in M:
+    for i in M:  # Find a single one in a frequency map
         if M[i] < 2:
             return i
     return 0
@@ -25,9 +30,11 @@ def freq_map(A):
 
 def binary(A):
     """
+    Linear solution using binary logic.
+
     Complexity: O(n) time, no extra space (except the output)
     :param list[int] A: Input array
-    :return int:
+    :return int: Missing number
     """
     out = 0
     n = len(A)
