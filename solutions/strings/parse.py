e9214f947896e3f01f23a87f069f7896b9768ebc
Paul Burakov <pburakov@gmail.com>
2016-08-27 23:24:28 -0400

    nit picks

--- solutions/strings/parse.py
+++ solutions/strings/parse.py
@@ -1,16 +1,15 @@
 """
-Given a simple syntax using numbers and square brackets, parse a given expression into a
- complete string. Numbers represent number of times following character or expression
- shall be repeated. Default number of repetitions of a character or expression is 1,
- unless explicitly specified. Nested expressions are enclosed in square brackets.
+Given a simple syntax, using numbers and square brackets, parse a given expression into a
+ complete string. Numbers specify how many times following character or expression shall
+ be repeated. Default number of repetitions of a character or an expression is 1, unless
+ explicitly specified. Nested expressions are enclosed in square brackets.
 
 Input: `ab4c`. Output: `abcccc`
-Input: `1a1b0c`. Output: `ab`
-Input: `2[abc]`. Output: `abcabc`
+Input: `ab0c`. Output: `ab`
+Input: `2[a2bc]`. Output: `abbcabbc`
 Input: `2[ab3[cd]]x3yz`. Output: `abcdcdcdabcdcdcdxyyyz`
 
-I've been asked this problem when I was interviewing with Google, but couldn't find a
- complete solution on the web. So here is my stab at it.
+I've been asked this problem when I was interviewing with Google. Here is my stab at it.
 """
 
 
