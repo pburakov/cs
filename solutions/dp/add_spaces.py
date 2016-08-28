dfea84ff80d2f7535698c320d2e3df3a0b063ef5
Paul Burakov <pburakov@gmail.com>
2016-08-28 12:27:27 -0400

    comments and description

--- solutions/dp/add_spaces.py
+++ solutions/dp/add_spaces.py
@@ -1,15 +1,25 @@
 """
+Imagine that a malicious virus has erased all whitespace characters in your text files.
+ Given a dictionary of words, you want to recreate valuable information with as less
+ unmatched words as possible. All inputs are lowercase, punctuation is ignored.
+
 Dictionary: {"this", "is", "favorite", "food"}
 Source string = "thisismikesfavoritefood"
-Output = "this is mikes favorite food"
+Output = "this is mikes favorite food" (5 unmatched characters in word "mikes")
+
+This problem is described in detail in Gayle McDowell's book "Cracking the Coding
+ Interview".
 """
 
 
 def solution(S, D):
     """
-    :param str S:
-    :param dict D:
-    :return str:
+    Add spaces problem solver.
+
+    Complexity: O(2^n) without memoization optimization (not implemented)
+    :param str S: Input string
+    :param set D: Dictionary of words
+    :return str: Output string with whitespaces in place
     """
     _, M = spaces(S, D)
     return reconstruct(S, M)
@@ -22,19 +32,25 @@ Auxiliary routines used in the solution
 
 def spaces(S, D, M=None, k=0):
     """
-    :param str S:
-    :param dict D:
-    :param list[bool] M:
-    :param int k:
-    :return (int, list[bool]):
+    Builds map of whitespace placement with minimum unmatched characters.
+
+    Algorithm exhaustively tries all whitespaces positions and then counts characters of
+     the words that matched the dictionary.
+
+    Complexity: O(2^n), can be optimized using memoization (not implemented)
+    :param str S: Input string
+    :param set D: Dictionary of words
+    :param list[bool] M: Intermediate map of whitespaces positions
+    :param int k: Rolling index (used in recursion)
+    :return (int, list[bool]): Tuple of remaining unmatched characters and
     """
     n = len(S)
-    if M is None:
+    if M is None:  # Initialize intermediate whitespace map
         M = [False] * n
     if k == n:
         word = ''
         matched = 0
-        for i in range(0, n):
+        for i in range(0, n):  # Count all characters of matched words
             word += S[i]
             if M[i] is True:
                 if word in D:
@@ -42,9 +58,9 @@ def spaces(S, D, M=None, k=0):
                 word = ''
         return n - matched, M.copy()
     for i in range(k, n):
-        M[i] = True
+        M[i] = True  # Put whitespace
         with_space, map1 = spaces(S, D, M, k + 1)
-        M[i] = False
+        M[i] = False  # Remove whitespace
         without_space, map2 = spaces(S, D, M, k + 1)
         if with_space < without_space:
             return with_space, map1
@@ -54,9 +70,12 @@ def spaces(S, D, M=None, k=0):
 
 def reconstruct(S, M):
     """
-    :param str S:
-    :param list[bool] M:
-    :return str:
+    Reconstruct solution based on the whitespace map.
+
+    Complexity: O(n)
+    :param str S: Input string
+    :param list[bool] M: Whitespace map
+    :return str: Output string with whitespaces in place
     """
     out = []
     word = ''
