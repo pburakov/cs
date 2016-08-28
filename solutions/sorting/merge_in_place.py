6ad502e2ae14df01477977564c2769079865bac7
Paul Burakov <pburakov@gmail.com>
2016-08-27 22:53:30 -0400

    formatted description

--- solutions/sorting/merge_in_place.py
+++ solutions/sorting/merge_in_place.py
@@ -1,7 +1,9 @@
 """
-Given two sorted arrays `A` and `B`, with array `B` having additional memory allocated in its tail to store elements from both `A` and `B`, merge both arrays in place.
+Given two sorted arrays `A` and `B`, with array `B` having additional memory allocated in
+ its tail to store elements from both `A` and `B`, merge both arrays in place.
 
-Example: `merge(A=[1, 250, 600], B=[100, 200, 300, 0, 0, 0]) -> B=[1, 100, 200, 250, 300, 600]`
+Example:
+ `merge(A=[1, 250, 600], B=[100, 200, 300, 0, 0, 0]) -> B=[1, 100, 200, 250, 300, 600]`
 """
 
 
