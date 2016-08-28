9c68c347379d10f8e32147ebcfa8ccc41cd51c42
Paul Burakov <pburakov@gmail.com>
2016-08-27 22:52:13 -0400

    added string parse google problem

--- solutions/strings/parse.py
+++ solutions/strings/parse.py
@@ -1,41 +1,53 @@
 """
-I've been asked this problem when I was interviewing at Google. Given a simple syntax using numbers and square brackets, parse a given expression into a complete string.
+Given a simple syntax using numbers and square brackets, parse a given expression into a
+ complete string. Numbers represent number of times following character or expression
+ shall be repeated. Expressions are enclosed in square brackets.
 
 Input: `ab4c`. Output: `abcccc`
 Input: `1a1b0c`. Output: `ab`
 Input: `2[abc]`. Output: `abcabc`
 Input: `2[ab3[cd]]x3yz`. Output: `abcdcdcdabcdcdcdxyyyz`
+
+I've been asked this problem when I was interviewing with Google, but couldn't find a
+ complete solution on the web. So here is my stab at it.
 """
 
 
 def solution(S):
     """
+    Parse string problem solver.
 
-    :param str S:
-    :return str:
+    Complexity: O(n*b) where `b` is number of matched opening brackets.
+    :param str S: Input expression
+    :return str: Final string
     """
     parse_tree = tokenize(S)
     return reconstruct(parse_tree)
 
 
 """
-Auxiliary routines used in the solution
+Auxiliary routines and constants used in the solution
 """
 
 
 def tokenize(S):
     """
+    Tokenize the string and build the parse tree.
 
     Tokenization algorithm is based on a finite string automata. It has 3 states:
-     a) Parse digits into integer values (collected number is kept as number of pattern of repetitions for the next state)
+     a) Parse digits into integer values (collected number is kept as number of pattern
+      of repetitions for the next state)
      b) Tokenize parenthesized expression (expression is recursively tokenized)
      c) Tokenize single character
 
-    State of the automata is triggered based on a character encountered. State is held until another trigger is occurred.
+    State of the automata is triggered based on a character encountered. State is held
+     until another trigger is occurred. Result of this algorithm is a list representation
+     of a parse tree, containing tokenized characters and repeated expressions.
 
-    Complexity: O(n*b) where `b` is number of opening brackets that spawns nested string traversal
-    :param str S:
-    :return str:
+    Complexity: O(n*b) where `b` is number of matched opening brackets that may spawn
+     recursive expression traversal.
+    :param str S: Input string
+    :return list: Parse tree list representation
     """
     n = len(S)
     buffer = []
@@ -74,10 +86,10 @@ def tokenize(S):
 
 def reconstruct(T):
     """
-    Builds final string off a list of tokens.
+    Builds final string off a parse tree.
 
     Complexity: O(n) where `n` is length of a final string
-    :param list T: Tokenized expression
+    :param list T: Parse tree of a tokenized expression
     :return str: Final string
     """
     out = ''
@@ -88,6 +100,6 @@ def reconstruct(T):
     return out
 
 
-NUMBERS = "0123456789"
+NUMBERS = set("0123456789")
 OPENING_BRACKET = '['
 CLOSING_BRACKET = ']'
