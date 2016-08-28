dc17e2de164900ecc034797e347564cf7ad257ef
Paul Burakov <pburakov@gmail.com>
2016-08-28 13:20:57 -0400

    description and comments

--- solutions/queues_stacks/queue_of_two_stacks.py
+++ solutions/queues_stacks/queue_of_two_stacks.py
@@ -1,34 +1,54 @@
+"""
+Implement a queue using two stacks.
+"""
+
+
 class Queue:
     def __init__(self):
         """
         Implementation of a Queue using two stacks.
 
-        Implementation details:
-        Let stack 1 serve as a main storage
-        Let stack 2 be used as an aux storage to rearrange items on dequeue
-        To keep aux stack from redundant rearrangement, store a flag that it's safe to use it
-        Stacks are implemented using Python lists (to save coding time). append() and pop() operations are O(1)
+        Implementation details: Let stack 1 serve as a main storage. Let stack 2 be used
+         as an auxiliary storage that we're going to use to rearrange items on dequeue.
+         To keep aux stack from redundant rearrangement, store a flag that it's safe to
+         use it.
+
+        Stacks are implemented using Python lists (to save us some coding time).
+         `append()` and `pop()` operations are O(1).
         """
         self.main = []
         self.aux = []
         self.use_aux = False
 
     def enqueue(self, x):
-        if self.use_aux is True:
+        """
+        Puts new item into the queue.
+
+        Complexity: O(n) where `n` is the current number of items in the queue.
+        :param object x: Object to insert
+        :return None: Queue is updated
+        """
+        if self.use_aux is True:  # Rearrangement is required
             n = len(self.aux)
             self.main.clear()
-            for i in range(n - 1, -1, -1):  # rearranging in reverse order
+            for i in range(n - 1, -1, -1):  # Rearranging in reverse order
                 self.main.append(self.aux[i])
         self.main.append(x)
         self.use_aux = False
 
     def dequeue(self):
+        """
+        Removes an item from the head of the queue.
+
+        Complexity: O(n) where `n` is the current number of items in the queue.
+        :return object: Removed object.
+        """
         n = len(self.main)
-        if self.use_aux is False:
+        if self.use_aux is False:  # Rearrangement is required
             self.aux.clear()
-            for i in range(n - 1, -1, -1):  # adding in reverse order
+            for i in range(n - 1, -1, -1):  # Rearranging in reverse order
                 self.aux.append(self.main[i])
-        # now it's safe to use aux for the next operation unless enqueue() is invoked
+        # Now it's safe to use aux for the next operation unless enqueue() is invoked
         self.use_aux = True
         x = self.aux.pop()
         return x
