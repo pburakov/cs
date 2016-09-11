bffd3bc866cd5a3d232484081194c8ddad594acc
Paul Burakov <pburakov@gmail.com>
2016-09-11 12:20:43 -0400

    fixed `-` character

--- solutions/arrays/max_sum_subarray.py
+++ solutions/arrays/max_sum_subarray.py
@@ -1,8 +1,8 @@
 """
 The maximum subarray problem is the task of finding the contiguous subarray within a
  one-dimensional array of numbers which has the largest sum. For example, for the
- sequence of values `[−2, 1, −3, 4, −1, 2, 1, −5, 4]`; the contiguous subarray with the
- largest sum is `[4, −1, 2, 1]`, with the sum `6`.
+ sequence of values `[-2, 1, -3, 4, -1, 2, 1, -5, 4]`; the contiguous subarray with the
+ largest sum is `[4, -1, 2, 1]`, with the sum `6`.
 
 Linear-time solution is named after Jay Kadane who first found it in 1984.
 """
