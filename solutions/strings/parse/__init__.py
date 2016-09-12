68ae5b1e7eadcc36aebcbdd1d10711a3a8cb9bdf
Paul Burakov <pburakov@gmail.com>
2016-09-11 21:29:06 -0400

    packaged parse syntax

--- solutions/strings/parse/__init__.py
+++ solutions/strings/parse/__init__.py
@@ -1,18 +1,3 @@
-"""
-Given a simple syntax, using numbers and square brackets, parse a given expression into a
- complete string. Numbers specify how many times following character or expression shall
- be repeated. Default number of repetitions of a character or an expression is 1, unless
- explicitly specified. Nested expressions are enclosed in square brackets.
-
-Input: `ab4c`. Output: `abcccc`
-Input: `ab0c`. Output: `ab`
-Input: `2[a2bc]`. Output: `abbcabbc`
-Input: `2[ab3[cd]]x3yz`. Output: `abcdcdcdabcdcdcdxyyyz`
-
-I've been asked this problem when I was interviewing with Google. Here is my stab at it.
-"""
-
-
 def solution(S):
     """
     Parse string problem solver.
