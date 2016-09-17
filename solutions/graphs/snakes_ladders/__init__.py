e228a0cdc2e1f39eed69978d903e16b6755f7633
Paul Burakov <pburakov@gmail.com>
2016-09-17 12:34:13 -0400

    packaged snakes ladders

--- solutions/graphs/snakes_ladders/__init__.py
+++ solutions/graphs/snakes_ladders/__init__.py
@@ -1,14 +1,3 @@
-"""
-Implement an algorithm that will find the minimum amount of dice throws required to win
- "Snake and Ladders" game.
-
-Game board is represented by an array with non-zero values representing index where the
- player will have to jump if he/she lands on that cell.
-
-Example: `M=[0, 0, 0, 7, 2, 0, 0, 0, 5, 0]`, Output: `2`.
-"""
-
-
 def solution(M):
     """
     Snake and Ladders minimum moves solver.
