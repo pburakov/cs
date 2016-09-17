564589b5fbd65693119a27c8ee29d6adcd1d7d09
Paul Burakov <pburakov@gmail.com>
2016-09-17 11:25:28 -0400

    packaged add spaces

--- solutions/dp/add_spaces/__init__.py
+++ solutions/dp/add_spaces/__init__.py
@@ -1,17 +1,3 @@
-"""
-Imagine that a malicious virus has erased all whitespace characters in your text files.
- Given a dictionary of words, you want to recreate valuable information with as less
- unmatched words as possible. All inputs are lowercase, punctuation is ignored.
-
-Dictionary: {"this", "is", "favorite", "food"}
-Source string = "thisismikesfavoritefood"
-Output = "this is mikes favorite food" (5 unmatched characters in word "mikes")
-
-This problem is described in detail in Gayle McDowell's book "Cracking the Coding
- Interview".
-"""
-
-
 def solution(S, D):
     """
     Add spaces problem solver.
---
+++ solutions/dp/add_spaces/test.py
@@ -0,0 +1,12 @@
+import unittest
+from solutions.dp.add_spaces import *
+
+
+class TestSolution(unittest.TestCase):
+    def test(self):
+        d1 = {"this", "is", "favorite", "food"}
+        s1 = "thisismikesfavoritefood"
+        d2 = {"brother", "looked", "just", "like", "her"}
+        s2 = "jesslookedjustliketimherbrother"
+        self.assertEqual(solution(s1, d1), "this is mikes favorite food")
+        self.assertEqual(solution(s2, d2), "jess looked just like tim her brother")
