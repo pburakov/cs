c10fb5abb989a9429ddc8d0e6842a421819f316b
Paul Burakov <pburakov@gmail.com>
2016-09-16 22:12:28 -0400

    package merge in place

--- solutions/sorting/merge_in_place/__init__.py
+++ solutions/sorting/merge_in_place/__init__.py
@@ -1,13 +1,4 @@
-"""
-Given two sorted arrays `A` and `B`, with array `B` having additional memory allocated in
- its tail to store elements from both `A` and `B`, merge both arrays in place.
-
-Example:
- `merge(A=[1, 250, 600], B=[100, 200, 300, 0, 0, 0]) -> B=[1, 100, 200, 250, 300, 600]`
-"""
-
-
-def solution(A, B):
+def sort(A, B):
     """
     Merge routine for the solution.
 
