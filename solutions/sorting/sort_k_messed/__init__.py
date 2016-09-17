4b25507ad423f05276d703faadc5f91fde7b7edf
Paul Burakov <pburakov@gmail.com>
2016-09-16 22:27:49 -0400

    package sort k messed

--- solutions/sorting/sort_k_messed/__init__.py
+++ solutions/sorting/sort_k_messed/__init__.py
@@ -1,12 +1,3 @@
-"""
-Given an array `A` of length `n` where each element is at most `k > 1` places away from
- its sorted position, plan and code an efficient algorithm to sort `A`.
-
-Example: With `A=[3, 1, 2, 5, 4, 7, 6]` and `k=2` the element belonging to index 3 in the
- sorted array, may be at indices 4, 5, 6, 7 or 8 on the given array.
-"""
-
-
 def sort(A, k):
     """
     Sort k-messed array.
---
+++ solutions/sorting/sort_k_messed/test.py
@@ -0,0 +1,9 @@
+import unittest
+from solutions.sorting.sort_k_messed import *
+
+
+class TestSolution(unittest.TestCase):
+    def test(self):
+        a = [3, 1, 2, 5, 4, 7, 6]
+        sort(a, 2)
+        self.assertEqual(a, [1, 2, 3, 4, 5, 6, 7])
