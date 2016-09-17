86a4288c3eefa4ebcd30f8ff8bbb55b6f72a27f0
Paul Burakov <pburakov@gmail.com>
2016-09-17 12:37:54 -0400

    packaged word ladder

--- solutions/graphs/word_ladder/__init__.py
+++ solutions/graphs/word_ladder/__init__.py
@@ -1,17 +1,3 @@
-"""
-This is a word production sequence problem, better known as word ladder. It can be found
- in many different flavours. Although it involves strings, it is a great example how
- simple graph algorithms can be used to solve various seemingly unrelated problems.
-
-You are given a dictionary of words of the same lengths, consisting of lowercase letters.
- Example: `{"hot","hit","dot","dog","cog","lot","log"}`. Given a starting word and a
- target word, find the shortest available transformation sequence, if such exists. Two
- rules have to be maintained:
-  - at each step only a single letter can be changed at a time,
-  - every word in a sequence must exist in a dictionary.
-
-Example ladder: `"hit" -> "hot" -> "dot" -> "dog" -> "cog"` of length of 5.
-"""
 from basic_data_structures.fifo import Queue
 from basic_data_structures.fifo import enqueue, dequeue, next as peek
 
