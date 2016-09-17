2f1357aac0af21123732fee6996d851644985f9a
Paul Burakov <pburakov@gmail.com>
2016-09-17 13:20:57 -0400

    added description

--- solutions/dp/matrix_paths/__init__.py
+++ solutions/dp/matrix_paths/__init__.py
@@ -2,7 +2,23 @@ def backtrack(M, x=None, y=None):
     """
     Matrix path recursive solver.
 
-    Complexity: O((n+m-2)!/((n-1)!(m-1)!), or n-choose-m
+    Imagine that we're solving this problem for the matrix of size `4`x`3` with only free
+     cells. Observe that any path will take `m + n - 2 = 5` moves corner-to-corner, for
+     example: DDRRR, RRRDD, RDRDR (D=down, R=right) and so on. Observe that each route
+     will have 2 D's and 3 R's. Which looks like a permutation problem.
+
+    If matrix has nothing but free cells (no `1`s), the solution would be as easy as
+     solving n-choose-k forumla: `5!/(2!3!) = 10`. Since some of the cells are not
+     traversable we can solve it using backtracking for a reduced number of possible
+     moves.
+
+    We have only three cases:
+     a) we reached destination
+     b) we reached an edge
+     c) we move right or down
+
+    Complexity: O((n+m-2)!/((n-1)!(m-1)!), is proportional to the output, or n-choose-m
+     formula for combinations without repetitions.
     :param list[list[int]] M:
     :param int x: Vertical index (used in recursion)
     :param int y: Horizontal index (user in recursion)
@@ -10,11 +26,11 @@ def backtrack(M, x=None, y=None):
     """
     if x is None or y is None:  # Initialize coordinates for bottom-up solution
         x, y = len(M) - 1, len(M[0]) - 1
-    if x == y == 0:
+    if x == y == 0:  # Destination
         return 1
-    elif x < 0 or y < 0 or M[x][y] == 1:
+    elif x < 0 or y < 0 or M[x][y] == 1:  # Edge
         return 0
-    else:
+    else:  # Free cell, two options where to move
         return backtrack(M, x, y - 1) + backtrack(M, x - 1, y)
 
 
