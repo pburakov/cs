086fecafbbc20004bab860e40f0c997f14a39b77
Paul Burakov <pburakov@gmail.com>
2016-09-17 12:13:10 -0400

    packaged longest increasing

--- solutions/dp/lis/__init__.py
+++ solutions/dp/lis/__init__.py
@@ -1,14 +1,3 @@
-"""
-Longest increasing sequence, or LIS, if a sequence of increasing, not necessarily
- continuous, elements of an input set in order of their appearance. Single element is LIS
- by definition.
-
-Example: `A=[2, 4, 3, 5, 1, 7, 6, 9, 8, 6]`, `output=[2, 3, 5, 6, 8]`.
-
-This algorithm is described in detail in the book "Algorithm Design Manual" by S. Skiena.
-"""
-
-
 def dp(A):
     """
     Returns longest increasing sequence of a set.
