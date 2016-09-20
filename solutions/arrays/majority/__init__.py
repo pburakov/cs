60d79d35307cdb1ed3e52ac11612199e2b7d6382
Paul Burakov <pburakov@gmail.com>
2016-09-20 10:06:32 -0400

    added description

--- solutions/arrays/majority/__init__.py
+++ solutions/arrays/majority/__init__.py
@@ -2,6 +2,14 @@ def find(A):
     """
     Finds majority element in the list.
 
+    This solution implements Moore’s Voting Algorithm which is a two step process. First
+     we get an element occurring most of the time in the array. By counting we evaluate
+     if our candidate is supported by more occurring elements. We reset the counter if it
+     went below zero.
+
+    Statistically, the first step may give us a false positive. On the second step we
+     check if the element obtained from above step is in fact a majority element.
+
     Complexity: O(n), no extra space is used
     :param list[object] A: Input list
     :return Optional[object]: Majority element or None if not found
