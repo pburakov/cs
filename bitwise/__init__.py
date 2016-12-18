62f71f8c284055d43c1061eb68c389cec9c7bbc2
Paul Burakov <pburakov@gmail.com>
2016-12-18 12:06:24 -0500

    bitwise module sphinx compatible

--- bitwise/__init__.py
+++ bitwise/__init__.py
@@ -1,46 +1,51 @@
 """
+Bitwise Operations
+==================
+
 Bit-level operations are essential, since this is how data primitives are presented and
- operated in computer's memory.
+operated in computer's memory.
 
-For example, integer `6` in 8-bit binary representation is `0000 0110`. Each register
- represent whether the next power of 2 should be added to the total value, counting from
- right (2<sup>0</sup>) to left (2<sup>8</sup>).
+For example, integer 6 in 8-bit binary representation is ``0000 0110``. Each register
+represent whether the next power of 2 should be added to the total value, counting from
+right (:math:`2^0`) to left (:math:`2^8`).
 
-Maximum value in 8-bit representation is 2<sup>8</sup> - 1 or `255` or `1111 1111`. Modern
- languages use 16-, 32-bit integer type, such as `unsigned int` and `long` in C. Python 3
- has unified `int` type which has no conventional bounds.
+Maximum value in 8-bit representation is :math:`2^8 - 1 = 255` or ``1111 1111``. Modern
+languages use 16-, 32-bit integer type, such as ``int`` and ``long`` in C. Python 3 has
+unified ``int`` type which has no conventional bounds.
 
-**Bitwise operators** (`&`, `|`, `^`, `~`, `>>`, `<<` ) operate on numbers treating them
- not as a single value, but as a string of bits.
+Bitwise operators (``&``, ``|``, ``^``, ``~``, ``>>``, ``<<`` ) operate on numbers treating
+them not as a single value, but as a string of bits.
 
-**AND** operation (`&`) is used in **bit masking**. Masking to `1` allows to query bit's
- status, i.e. `5 & 1` is `1` (`0101 & 0001 = 0001`), in other words we verify that the
- least significant bit (rightmost bit representing least power of 2, **LSB** for short) is
- set. Similarly, bit masking to `0` allows switching the bit off.
+AND operation (``&``) is used in **bit masking**. Masking to 1 allows to query bit's
+status, i.e. ``5 & 1 = 1`` (``0101 & 0001 = 0001``), in other words we verify that the
+least significant bit (rightmost bit representing least power of 2, **LSB** for short) is
+set. Similarly, bit masking to 0 allows switching the bit off.
 
-**XOR** is a logical operation that outputs true only when inputs differ (one is true, the
- other is false. Based on this definition, bitwise XOR-to-1 is commonly used for switching
- bit state, i.e. `5 ^ 15` is `10` (`0101 ^ 1111 = 1010`).
+XOR is a logical operation that outputs true only when inputs differ (one is true, the
+other is false). Based on this definition, bitwise XOR-to-1 is commonly used for switching
+bit state, i.e. ``5 ^ 15 = 10`` (``0101 ^ 1111 = 1010``).
 
 **Bit shifting** will shift all the bits in a word by arbitrary number of positions.
- Shifting *n* times is roughly the analog of multiplying (`<<`) or dividing (`>>`) the
- value by 2<sup>*n*</sup>. During left shift an unset bit is "shifted-in" at the least
- significant side. During right shift the least significant bit is discarded. For
- example, `5 >> 1 = 2` (`0101 >> 1 = 0010`).
+Shifting *n* times is roughly the analog of multiplying (``<<``) or dividing (``>>``) the
+value by :math:`2^n`. During left shift an unset bit is "shifted-in" at the least
+significant side. During right shift the least significant bit is discarded. For example,
+``5 >> 1 = 2`` (``0101 >> 1 = 0010``).
 
 Various combinations of these operations allow to quickly modify values in the memory.
 """
 
 
 def count1(x):
-    """
-    Counts number of set bits in an integer word.
+    """Counts number of set bits in an integer word.
 
     The method uses the properties of bit masking to a single bit as well as bit shifting.
 
-    Complexity: O(n), where n is the total number of bits in a word.
-    :param int x: Input integer
-    :return int: Number of set bits in an integer word
+    Complexity:
+        :math:`O(n)`, where :math:`n` is the total number of bits in a word.
+
+    :param int x: Input integer.
+    :return: Number of set bits in a binary integer word.
+
     """
     bits = 0
     while x:
@@ -50,15 +55,17 @@ def count1(x):
 
 
 def parity(x):
-    """
-    Returns parity of a binary word.
+    """Returns parity of a binary word.
 
     Parity is 1 if a number of set bits in a binary word is odd. Parity checks are used
      to detect single-bit errors in data stream or storage.
 
-    Complexity: O(n), where *n* is number of bits in a word
-    :param int x: Input integer
-    :return int: Parity of an input integer
+    Complexity:
+        :math:`O(n)`, where :math:`n` is number of bits in a word.
+
+    :param int x: Input integer.
+    :return: Parity of an input integer, 1 or 0.
+
     """
     p = 0
     while x:
@@ -68,16 +75,18 @@ def parity(x):
 
 
 def swap(a, b):
-    """
-    Swaps two integers in a tuple without using extra memory.
+    """Swaps two integers in a tuple without using extra memory.
 
     This algorithm is based on properties of XOR operation. Binary values are "flipped"
      twice against each other.
 
-    Complexity: O(1), no extra space.
+    Complexity:
+        :math:`O(1)`, no extra space.
+
     :param int a: Input integer
     :param int b: Input integer
-    :return tuple: Tuple of two swapped integers
+    :return: Tuple of two swapped integers
+
     """
     a ^= b
     b ^= a
