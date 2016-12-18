be1e6ffaf742aa934a767eba8d50feda2d2c5c31
Paul Burakov <pburakov@gmail.com>
2016-12-18 12:42:36 -0500

    combinatorial module sphinx compatible

--- combinatorial/optimization.py
+++ combinatorial/optimization.py
@@ -1,41 +1,44 @@
 """
-Optimization problems can have many possible solutions, each of which has a value, and
- we wish to find one with the optimal (minimum or maximum) value. These problems are
- typically solved with the use of **dynamic programming** or DP.
+Dynamic Programming
+===================
+
+Optimization problems can have many possible solutions, each of which has a value, and we
+wish to find one with the optimal (minimum or maximum) value. These problems are typically
+solved with the use of **dynamic programming** or DP.
 
 There two key attributes that a problem must have in order for DP to be applicable:
- *overlapping sub-problems* and *optimal substructure*. DP usually involves the use of
- some sort of **memoization** technique. Solutions are built on sub-problems that already
- have been solved to optimality, or are being solved repeatedly.
+overlapping sub-problems and optimal substructure. DP usually involves the use of some sort
+of **memoization** technique. Solutions are built on sub-problems that already have been
+solved to optimality, or are being solved repeatedly.
 
 DP solution is not obvious. Suggested first step is finding a recursive solution first.
- Second step is finding the state or the configuration that can be saved and reused. A
- helpful technique is describing state transitions as a graph problem to which a
- shortest or longest path algorithm can be applied. In fact Richard Bellman, one of the
- contributors to **Bellman-Ford** algorithm, was the one who developed the idea of
- dynamic programming.
+Second step is finding the state or the configuration that can be saved and reused. A
+helpful technique is describing state transitions as a graph problem to which a shortest or
+longest path algorithm can be applied. In fact Richard Bellman, one of the contributors to
+Bellman-Ford algorithm, was the one who developed the idea of dynamic programming.
 
 Some famous DP problems include: Knapsack, Rod Cutting, Edit Distance, Longest Common
- Subsequence, Longest Increasing Sequence, Coin Change, Matrix Multiplication, Text
- Justification, Max Cost Grid Path and many more. Some of them can be found in the
- [Solutions](/problems) section.
+Subsequence, Longest Increasing Sequence, Coin Change, Matrix Multiplication, Text
+Justification, Max Cost Grid Path and many more.
 
-Below are some basic DP templates, that help introducing the concept. It is essential to
- thoroughly examine these, including comments. Keep in mind, that practical applications
- of DP go well beyond these examples and require substantial amount of practice.
+Below are some basic DP templates, that help introducing the concept. However, practical
+applications of DP go well beyond these examples and require substantial amount of practice.
 """
 
+
 def memoize(f):
-    """
-    Memoization function decorator.
+    """Memoization function decorator.
 
-    Loads value from cache if it was already computed. Arguments of a function need to
-     be hashable to be stored in cache. This is Python-specific implementation,
-     applicable to simple cases of recursive functions.
+    Loads value from cache if it was already computed. Arguments of a function need to be
+    hashable to be stored in cache. This is Python-specific implementation, applicable to
+    simple cases of recursive functions.
 
-    Complexity: O(1) for storage and retrieval.
-    :param (tuple)->(Any) f: Function
-    :return Any: Return value of a function `f`
+    Complexity:
+        `O(1)` for storage and retrieval.
+
+    :param (tuple)->(Any) f: Function.
+    :return: Return value of a function.
+
     """
     cache = {}
 
@@ -49,18 +52,20 @@ def memoize(f):
 
 @memoize
 def fib(n):
-    """
-    Calculates n-th Fibonacci number recursively.
+    """Calculates n-th Fibonacci number recursively.
 
     Although Fibonacci formula is recursive in nature, simple recursion, as in many
-     recurrent calculation problems, is not an optimal solution for it. This is a
-     textbook Fibonacci example, optimized with memoization by Python decorator.
+    recurrent calculation problems, is not an optimal solution for it. This is a textbook
+    Fibonacci example, optimized with memoization by Python decorator.
 
-    Complexity: O(2^n) without optimization as each call invokes two more recursive
-     calls. Time is reduced to linear O(n) with the use of memoization, which takes
-     additional O(n) space.
-    :param int n: Number in Fibonacci sequence
-    :return int: `n`-th Fibonacci number
+    Complexity:
+        :math:`O(2^n)` without optimization as each call invokes two more recursive calls.
+        Time is reduced to linear :math:`O(n)` with the use of memoization, which takes
+        additional :math:`O(n)` space.
+
+    :param int n: Number in Fibonacci sequence.
+    :return: :math:`n`-th Fibonacci number.
+
     """
     if n < 0:
         raise ValueError("Invalid input")
@@ -73,28 +78,29 @@ def fib(n):
 
 
 def fib_dp(n):
-    """
-    Calculates n-th Fibonacci number using DP.
+    """Calculates n-th Fibonacci number using DP.
 
-    This is a canonical DP example. Although it is based on a recursive solution, it
-     uses different memoization technique by keeping calculated values in an array. Most
-     DP algorithms make use of arrays to store solved states, including multidimensional
-     arrays.
+    This is a canonical DP example. Although it is based on a recursive solution, it uses
+    different memoization technique by keeping calculated values in an array. Most DP
+    algorithms make use of arrays to store solved states, including multidimensional arrays.
 
-    This template is used in many DP solutions. To better understand it, try to
-     represent its operation as a topologically sorted graph and think about the
-     relations between vertices. In this case there's only one way to get to the `i`-th
-     vertex, which is to calculate vertices `i-2` and `i-1`. There's also the same cost
-     of getting there, consider it to be a weight of `1`. In many DP problems there is
-     more than one way to reach a vertex with different costs associated with each.
+    This template is used in many DP solutions. To better understand it, try to represent
+    its operation as a topologically sorted graph and think about the relations between
+    vertices. In this case there's only one way to get to the :math:`i`-th vertex, which
+    is to calculate vertices :math:`i-2` and :math:`i-1`. There's also the same cost of
+    getting there, consider it to be a weight of :math:`1`. In many DP problems there is
+    more than one way to reach a vertex with different costs associated with each.
 
     Lastly, in many DP problems saved calculations usually are not so obvious as in this
-     example. Often they need to be interpreted or "traced back" in order to return a
-     final solution for a given problem.
+    example. Often they need to be interpreted or "traced back" in order to return a final
+    solution for a given problem.
 
-    Complexity: O(n) with O(n) additional storage space.
-    :param int n: Number in Fibonacci sequence
-    :return int: `n`-th Fibonacci number
+    Complexity:
+        :math:`O(n)` with :math:`O(n)` additional storage space.
+
+    :param int n: Number in Fibonacci sequence.
+    :return: :math:`n`-th Fibonacci number.
+
     """
     DP = [0] * (n + 1)  # Allocating table to hold calculated values
     DP[1] = 1
--- combinatorial/search.py
+++ combinatorial/search.py
@@ -1,57 +1,62 @@
 """
+Combinatorial Search
+====================
+
 Many combinatorial search problems can be solved to optimality using exhaustive search
- techniques, although computational cost of such solutions can be enormous, even
- unfeasible. Time and space complexity of such problems are coherent with the size of its
- output. It can be predicted using counting theory formulas for **exhaustive search**.
+techniques, although computational cost of such solutions can be enormous, even unfeasible.
+Time and space complexity of such problems are coherent with the size of its output. It
+can be predicted using counting theory formulas for **exhaustive search**.
 
 This type of problems is often called **"n-choose-k"**, due to the binomial coefficient
- that denotes the number of *k*-combinations of a set of *n* elements.
+that denotes the number of :math:`k`-combinations of a set of :math:`n` elements.
 
 Combinatorial search algorithms use recursion with a **backtracking** technique. Recursive
- solutions resemble depth-first search algorithm where graph represents all possible
- configurations, or states. Backtracking is a systematic way to iterate through all the
- sates in a search space, while avoiding repetitions and bad configurations. Backtracking
- algorithm abandons a partial candidate as soon as it determines that it cannot possibly
- lead to a valid solution
+solutions resemble depth-first search algorithm where graph represents all possible
+configurations, or states. Backtracking is a systematic way to iterate through all the
+sates in a search space, while avoiding repetitions and bad configurations. Backtracking
+algorithm abandons a partial candidate as soon as it determines that it cannot possibly
+lead to a valid solution
 
 Description of common steps of a backtracking algorithm:
- 1. Define **base case** for recursion to finish. For instance, report a found solution or
-  report that algorithm has exhausted all possible solutions for given configuration.
- 2. Construct a new **solution candidate**, or several. For example, exclude, swap or
-  replace an element of a set.
- 3. Check if proposed solution does not violate problem conditions. This is where we
-  abandon a branch of a **recursive tree** (not shown in examples).
- 4. **Call itself** on new candidate or, if there are many, on each of the new candidates.
- 5. Undo updates made to a set (optional). This step occurs after the running process
-  exits the recursion (or **"backtracks"**).
+
+ - Define base case for recursion to finish. For instance, report a found solution or
+   report that algorithm has exhausted all possible solutions for given configuration.
+ - Construct a new solution candidate, or several. For example, exclude, swap or replace
+   an element of a set.
+ - Check if proposed solution does not violate problem conditions. This is where we
+   abandon a branch of a recursive tree (not shown in examples).
+ - Call itself on new candidate or, if there are many, on each of the new candidates.
+ - Undo updates made to a set (optional). This step occurs after the running process
+   exits the recursion (or "backtracks").
 
 This module contains implementation templates and techniques for some of the most common
- combinatorial search problems.
+combinatorial search problems.
 """
 
 
 def subsets(S, P, i=0, M=None):
-    """
-    Produces all subsets of a set.
+    """Produces all subsets of a set.
 
     Exhaustively searches all possible subsets of a set (Python list). Order is not
-     important. For example, `{1, 3}` and `{3, 1}` are the same subset.
+    important. For example, :math:`\{1, 3\}` and :math:`\{3, 1\}` are the same subset.
 
     At each step algorithm "decides" whether to include next element or not. Same
-     algorithm is invoked for each decision. The problem is solved in the same manner
-     as a permutation problem.
+    algorithm is invoked for each decision. The problem is solved in the same manner as a
+    permutation problem.
 
-    In this implementation the decisions table is represented as an `n`-sized bit map,
-     where bits `1` and `0` at `i`-th position determine whether `i`-th element should
-     be included or not.
+    In this implementation the decisions table is represented as an :math:`n`-sized bit
+    map, where bits ``1`` and ``0`` at :math:`i`-th position determine whether :math:`i`-th
+    element should be included or not.
 
-    Complexity: Theta(2^n) for time. Recursive tree is binary, for each of `n` elements
-     we decide whether we include that element in a subset or not.
-    :param list S: Input set
-    :param list P: Output list of subsets
-    :param int i: Element lookup index (used in recursion)
-    :param list M: Bitmap of a subset (used in recursion)
-    :return None: List `P` is populated
+    Complexity:
+        :math:`\Theta (2^n)` for time. Recursive tree is binary, for each of :math:`n`
+        elements we decide whether we include that element in a subset or not.
+
+    :param list S: Input set.
+    :param list P: Output list of subsets.
+    :param int i: Element lookup index (used in recursion).
+    :param list M: Bitmap of a subset (used in recursion).
+
     """
     n = len(S)
     if M is None:
@@ -68,20 +73,22 @@ def subsets(S, P, i=0, M=None):
 
 
 def permutations(S, P, i=0):
-    """
-    Produces all permutations of a set.
+    """Produces all permutations of a set.
 
     Permutation of a set contains all the same elements, but in a different order. This
-     algorithm exhaustively searches all possible combinations of elements in a set
-     (Python list) by continuously swapping pairs of elements using recursion, then
-     backtracks to the original state of a set.
+    algorithm exhaustively searches all possible combinations of elements in a set (Python
+    list) by continuously swapping pairs of elements using recursion, then backtracks to
+    the original state of a set.
 
-    Complexity: Theta(nn!) time. Space complexity is O(n!n) is proportional to the
-     output. There are `n!` permutations of a set of `n`-elements set by definition.
-    :param list S: Input set
-    :param list P: Output list of permutations
-    :param int i: Starting index (used in recursion)
-    :return None: List `P` is populated
+    Complexity:
+        :math:`\Theta (n!n)` time. Space complexity is :math:`O(n!n)` is proportional to
+        the output. There are :math:`n!` permutations of a set of :math:`n`-elements set
+        by definition.
+
+    :param list S: Input set.
+    :param list P: Output list of permutations.
+    :param int i: Starting index (used in recursion).
+
     """
     n = len(S)
     if i >= n - 1:
@@ -94,28 +101,29 @@ def permutations(S, P, i=0):
 
 
 def partitions(S, P, s=0, p=None):
-    """
-    Produces all possible partitions of a set.
+    """Produces all possible partitions of a set.
 
     Partitioned set contains such subsets that, when joined, they produce the original
-     set. Order of elements is preserved. For example a set `{a,b,c}` can be partitioned
-     into `{a,b,c}`, `{a}+{b,c}`, `{a,b}+{c}` or `{a}+{b}+{c}`.
+    set. Order of elements is preserved. For example a set :math:`\{a,b,c\}` can be
+    partitioned into :math:`\{a,b,c\}`, :math:`\{a\}+\{b,c\}`, :math:`\{a,b\}+\{c\}` or
+    :math:`\{a\}+\{b\}+\{c\}`.
 
     This algorithm is similar to subsets generation. Instead of deciding whether to
-     include an element into a partition, it decides whether the set should be "sliced"
-     at this point, starting at the beginning of the set. The remainder sub-partition
-     to the "right" of the slice, is added to the stack and same algorithm is called
-     on it.
+    include an element into a partition, it decides whether the set should be "sliced" at
+    this point, starting at the beginning of the set. The remainder sub-partition to the
+    "right" of the slice, is added to the stack and same algorithm is called on it.
 
     After sub-partitions have been computed, the algorithm "backtracks" and pops counted
-     elements of the partition from the stack. Then operation is repeated.
+    elements of the partition from the stack. Then operation is repeated.
 
-    Complexity: O(2^n) bounded by (2^n)/2 possible partitions
-    :param list S: Input set
-    :param list P: Output list of set partitions
-    :param int s: Starting index (used in recursion)
-    :param list p: Intermediate partition stack (used in recursion)
-    :return None: List `P` is populated
+    Complexity:
+        :math:`O(2^n)` bounded by :math:`^{2^n}/_2` possible partitions.
+
+    :param list S: Input set.
+    :param list P: Output list of set partitions.
+    :param int s: Starting index (used in recursion).
+    :param list p: Intermediate partition stack (used in recursion).
+
     """
     n = len(S)
     if p is None:
