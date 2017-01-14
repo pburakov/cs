99f7202753441ed04b9176463c6d1dd54468f8f0
Paul Burakov <pburakov@gmail.com>
2017-01-14 16:00:26 -0500

    moved swap problem

--- bitwise/__init__.py
+++ bitwise/__init__.py
@@ -72,23 +72,3 @@ def parity(x):
         p ^= x & 1  # Switch `p` if LSB is different from previously saved `p`
         x >>= 1
     return p
-
-
-def swap(a, b):
-    """Swaps two integers in a tuple without using extra memory.
-
-    This algorithm is based on properties of XOR operation. Binary values are "flipped"
-     twice against each other.
-
-    Complexity:
-        :math:`O(1)`, no extra space.
-
-    :param int a: Input integer
-    :param int b: Input integer
-    :return: Tuple of two swapped integers
-
-    """
-    a ^= b
-    b ^= a
-    a ^= b
-    return a, b
---
+++ problems/bitwise/swap.py
@@ -0,0 +1,25 @@
+"""
+Swap in-place
+
+Swaps two integers without using extra memory.
+"""
+
+
+def swap(a, b):
+    """Swaps two integers in a tuple without using extra memory.
+
+    This algorithm is based on properties of XOR operation. Binary values are "flipped"
+     twice against each other.
+
+    Complexity:
+        :math:`O(1)`, no extra space.
+
+    :param int a: Input integer
+    :param int b: Input integer
+    :return: Tuple of two swapped integers
+
+    """
+    a ^= b
+    b ^= a
+    a ^= b
+    return a, b
