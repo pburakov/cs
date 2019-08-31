35740fb03346cbce465dde725cb860f0cc92d3eb
Paul Burakov <pburakov@gmail.com>
2019-08-31 14:31:33 +0100

    optimize alphabet in word ladder problem

--- problems/graphs/word_ladder.py
+++ problems/graphs/word_ladder.py
@@ -2,13 +2,9 @@
 Word Ladder
 ===========
 
-This is a word production sequence problem, better known as word ladder. It can be found
-in many different disguises. It is a great example of how simple graph algorithms can be
-used to solve various seemingly unrelated problems.
-
-You are given a dictionary of words of the same lengths, consisting of lowercase letters.
-Given a starting word and a target word, find the shortest available transformation
-sequence, if such exists.
+You are given a dictionary of words of same lengths :math:`D`, consisting of lowercase
+letters. Given a starting word :math:`s` and a target word :math:`t`, find the shortest
+available transformation sequence, if such exists.
 
 Two rules have to be maintained:
 
@@ -17,112 +13,118 @@ Two rules have to be maintained:
 
 Example::
 
-    Input:
-    D = {"hot","hit","dot","dog","cog","lot","log"}
-    s = "hit"
-    t = "cog"
+    (D={"hot","hit","dot","dog","cog","lot","log"},
+     s="hit",
+     t="cog") -> ["hit","hot", "dot", "dog", "cog"]
 
-    Output: ["hit","hot", "dot", "dog", "cog"]
+This is a word production sequence problem, better known as the word ladder. It can be found
+in many different disguises. It is a great example of how simple graph algorithms can be
+used to solve various seemingly unrelated problems.
 
 """
 from basic.fifo import Queue
 from basic.fifo import enqueue, dequeue, next as peek
 
 
-def solution(D, s, t):
-    """Word ladder solver using BFS shortest path.
+def word_ladder(D, s, t):
+    """Computes the word production sequence.
 
-    This solution takes advantage of a property of BFS algorithm. If we follow parent
-    pointers (a vertex from which other vertex was visited) in a breadth-first tree, we
-    can reconstruct the shortest path between two vertices. Let every word in a dictionary
-    be a vertex in a graph. If we determine the path from :math:`s` to :math:`t` and
-    reconstruct it, we'll get the exact word production sequence.
-
-    Note, that this solution can be altered to return an integer distance without the path
-    reconstruction.
+    The solution takes an advantage of an important property of a BFS algorithm. If we
+    follow parent pointers (a vertex from which the other vertex was visited) from a target
+    vertex to a source vertex in a breadth-first tree, we can reconstruct the shortest path
+    between the two vertices. Let every word in a dictionary be a vertex in a graph. If we
+    determine the path from :math:`s` to :math:`t` and reconstruct it, we'll get the exact
+    word production sequence.
 
     Complexity:
-        :math:`O(|D|m)`, see the complexity of the BFS subroutine.
+        :math:`O(|D|m)`, where :math:`m` is the maximum length of words in a dictionary.
 
-    :param set D: Dictionary (set) of words.
-    :param str s: Starting string.
-    :param str t: Target string.
-    :return: Production sequence as a :data:`list`.
+    :param set D: Dictionary of words.
+    :param str s: Starting word.
+    :param str t: Target word.
+    :return: World production sequence.
 
     """
     stack, out = [], []  # Sequence stack and the output list
-    v = build(D, s, t)  # Return `v.distance` for path length
+    v = build_sequence(D, s, t)  # Return `v.distance` for path length
     # Getting production sequence backwards by reconstructing BFS path
     while v is not None:
-        stack.append(v.candidate_string)
-        v = v.parent
+        stack.append(v.s)
+        v = v.p
     # Building output by reversing a stack
     while len(stack) > 0:
         out.append(stack.pop())
     return out
 
 
-def build(D, s, t):
-    """Words production sequence path builder using BFS algorithm.
+def build_sequence(D, s, t):
+    """Builds word production sequence.
 
-    This BFS sub-routine connects vertices in a graph, writes parent pointers and distance
-    from the source string.
+    This BFS routine connects vertices in a search graph and computes distance from the
+    source string to a target string.
 
     Complexity:
-        :math:`O(|D|+|D|m) = O(|D|(1+m) = O(|D|m)`, where :math:`m` is the maximum length
-        of words used. Time devoted to construction of candidate strings is constant bound
-        to the alphabet size. The number of edges in the worst case is :math:`n^2`.
+        :math:`O(|D|m)`, where :math:`m` is the maximum length of words in a dictionary.
+        Time devoted to the construction of candidate words is constant, bound to the
+        available alphabet.
 
-    :param set D: Dictionary (set) of words.
-    :param str s: Source string.
-    :param str t: Target string.
-    :return: Vertex containing target string (if found).
+    :param set D: Dictionary of words.
+    :param str s: Source word.
+    :param str t: Target word.
+    :return: A target word vertex, if found.
 
     """
     n = len(D)
+    A = build_alphabet(D)  # Optimized alphabet uses only letters from the dictionary
     Q = Queue(n)
     D.remove(s)  # Remove used word from a dictionary
     enqueue(Q, Candidate(s, 0))
     while Q.length > 0:
         v = peek(Q)
-        if v.candidate_string == t:
+        if v.s == t:
             return v  # Target is found
-        cs = list(v.candidate_string)  # Converting to list, strings are immutable
-        # Try all possible permutations of a candidate string
-        for i in range(0, len(cs)):
-            for c in ALPHABET:
-                cs[i] = c
-                cs_as_str = "".join(cs)
-                if cs_as_str in D:
-                    D.remove(cs_as_str)
-                    enqueue(Q, Candidate(cs_as_str, v.distance + 1, v))
-            cs[i] = v.candidate_string[i]
+        w = list(v.s)  # Candidate word as a list of characters
+        # Try all possible permutations of characters in a candidate word
+        for i in range(0, len(w)):
+            for c in A:
+                w[i] = c
+                ws = "".join(w)  # Candidate word as a string
+                if ws in D:
+                    D.remove(ws)  # Remove used word from a dictionary
+                    enqueue(Q, Candidate(ws, v.d + 1, v))
+            w[i] = v.s[i]  # Revert explored character
         dequeue(Q)
     return None
 
 
-"""
-Constants and data structures used in the solution
-"""
+def build_alphabet(D):
+    """Returns a set of letters used in a dictionary.
+
+    :param set D: Dictionary of words.
+    :return: Set of letters used in the dictionary.
+
+    """
+    A = set()
+    for word in D:
+        for c in word:
+            A.add(c)
+    return A
 
 
 class Candidate:
-    """Graph vertex representing candidate string with a distance value.
+    """A candidate word vertex in a word ladder search space.
     """
 
     def __init__(self, s, d, p=None):
-        """Graph vertex representing candidate string with a distance value.
+        """A candidate word vertex in a word ladder search space.
 
-        :param str s: Candidate string.
-        :param int d: Distance.
+        :param str s: Candidate word.
+        :param int d: Distance from a source word.
         :param Candidate p: Parent vertex.
         """
-        self.candidate_string = s
-        self.distance = d
-        self.parent = p
+        self.s = s
+        self.d = d
+        self.p = p
 
     def __str__(self):
-        return self.candidate_string
-
-
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+        return self.s
